3786f241ff6aef58f6e60ced1dc47a4a1bb8267b
Mike Campbell <mike.campbell@getweave.com@Mikes-MacBook-Pro.local>
2020-05-09 22:27:10 -0600

    added colors

--- src/components/TextMap/TextMap.js
+++ src/components/TextMap/TextMap.js
@@ -4,11 +4,10 @@ const DIRECTION_UP = "DIRECTION_UP"
 const DIRECTION_DOWN = "DIRECTION_DOWN"
 const DIRECTION_LEFT = "DIRECTION_LEFT"
 const DIRECTION_RIGHT = "DIRECTION_RIGHT"
-const TILE_BOULDER = {letter: "B", canWalk: false}
-const TILE_SHORT_GRASS = {letter: ",", canWalk: true}
-const TILE_DIRT = {letter: ".", canWalk: true}
-
-
+const TILE_BOULDER = {backgroundColor: '#A52A2A', letter: <span style={{backgroundColor: '#A52A2A'}}>B</span>, canWalk: false }
+const TILE_SHORT_GRASS = {backgroundColor: '#3CB371', letter: <span style={{backgroundColor: '#3CB371'}}>,</span>, canWalk: true }
+const TILE_TALL_GRASS = {backgroundColor: '#3CB371', letter: <span style={{backgroundColor: '#3CB371'}}>w</span>, canWalk: true }
+const TILE_DIRT = {backgroundColor: '#FFF8DC', letter: <span style={{backgroundColor: '#FFF8DC'}}>.</span>, canWalk: true }
 
 function TextMap() {
 
@@ -16,34 +15,36 @@ function TextMap() {
 
   const [mapLetters, setMapLetters] = useState(
     [
-      [TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER],
-      [TILE_BOULDER, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_BOULDER],
-      [TILE_BOULDER, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_BOULDER],
-      [TILE_BOULDER, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_BOULDER],
-      [TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER],
+      [TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER],
+      [TILE_BOULDER, TILE_SHORT_GRASS, TILE_SHORT_GRASS, TILE_SHORT_GRASS, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_BOULDER],
+      [TILE_BOULDER, TILE_SHORT_GRASS, TILE_SHORT_GRASS, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_BOULDER],
+      [TILE_BOULDER, TILE_SHORT_GRASS, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_BOULDER],
+      [TILE_BOULDER, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_DIRT, TILE_TALL_GRASS, TILE_TALL_GRASS, TILE_BOULDER],
+      [TILE_BOULDER, TILE_DIRT, TILE_DIRT, TILE_TALL_GRASS, TILE_TALL_GRASS, TILE_TALL_GRASS, TILE_TALL_GRASS, TILE_BOULDER],
+      [TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER, TILE_BOULDER],
     ]
   )
 
-  const [mapText, setMapText] = useState('')
+  const [mapText, setMapText] = useState([])
 
   useEffect(() => {
-    let text = '';
+    let elements = [];
     let x = 0, y = 0;
     mapLetters.forEach(row => {
       row.forEach(cell => {
         if (y == userCoordinates.y && x == userCoordinates.x) {
-          text += '@';
+          elements.push(<span style={{backgroundColor: cell.backgroundColor}}>@</span>);
         } else {
-          text += cell.letter;
+          elements.push(cell.letter);
         }
         y++;
       })
       x++;
       y = 0;
-      text += '\n'
+      elements.push(<br />)
     })
 
-    setMapText(text)
+    setMapText(elements)
   }, [mapLetters, userCoordinates])
 
   React.useEffect(() => {
@@ -57,7 +58,6 @@ function TextMap() {
 
   const handleKeyDown = (e) => {
     const input = getInputFromKeyPress(e);
-
     if (36 < e.keyCode && e.keyCode < 41) { // arrow keys
       const direction = input;
       const cif = getCoordinatesInFront(userCoordinates.x, userCoordinates.y, direction)
@@ -78,16 +78,18 @@ function TextMap() {
 
   const getCoordinatesInFront = (x, y, direction) => {
     switch (direction) {
-      case DIRECTION_UP: return {x: x-1, y}
-      case DIRECTION_DOWN: return {x: x+1, y}
-      case DIRECTION_LEFT: return {x, y: y+1}
-      case DIRECTION_RIGHT: return {x, y: y-1}
+      case DIRECTION_UP: return { x: x - 1, y }
+      case DIRECTION_DOWN: return { x: x + 1, y }
+      case DIRECTION_LEFT: return { x, y: y + 1 }
+      case DIRECTION_RIGHT: return { x, y: y - 1 }
     }
   }
 
   return (
     <div className="TextMap">
-      <pre>{mapText}</pre>
+      <code>
+        {mapText}
+      </code>
     </div>
   );
 }
